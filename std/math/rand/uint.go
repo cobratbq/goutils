9fb555216d8e098354c4d9f8284ef0c5565097a0
Danny van Heumen <danny@dannyvanheumen.nl>
2020-01-29 14:26:37 +0100

    Simplify function name.

--- std/math/rand/uint.go
+++ std/math/rand/uint.go
@@ -4,10 +4,10 @@ import (
 	"math/rand"
 )
 
-// MathRandUint64NonZero generates a non-zero uint64 value. Due to the non-zero
+// Uint64NonZero generates a non-zero uint64 value. Due to the non-zero
 // requirement, the distribution of random numbers is NOT perfectly uniform.
 // TODO find nicer way to get random non-zero uint64.
-func MathRandUint64NonZero() uint64 {
+func Uint64NonZero() uint64 {
 	r := rand.Uint64()
 	if r == 0 {
 		r++
--- std/math/rand/uint_test.go
+++ std/math/rand/uint_test.go
@@ -4,18 +4,18 @@ import (
 	"testing"
 )
 
-func TestMathRandUint64NonZero(t *testing.T) {
-	v1 := MathRandUint64NonZero()
-	v2 := MathRandUint64NonZero()
-	v3 := MathRandUint64NonZero()
+func TestUint64NonZero(t *testing.T) {
+	v1 := Uint64NonZero()
+	v2 := Uint64NonZero()
+	v3 := Uint64NonZero()
 	if v1 == v2 || v2 == v3 || v1 == v3 {
 		t.FailNow()
 	}
 }
 
-func TestMathRandUint64NonZeroNotZero(t *testing.T) {
+func TestUint64NonZeroNotZero(t *testing.T) {
 	for i := 0; i < 100; i++ {
-		v := MathRandUint64NonZero()
+		v := Uint64NonZero()
 		if v == 0 {
 			t.Fail()
 		}
