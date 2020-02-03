b2052b636c3846c51441b0bc1bdbf7f4cfa24b71
Danny van Heumen <danny@dannyvanheumen.nl>
2020-02-03 14:22:14 +0100

    Fix unconventional parameter name.

--- std/builtin/map.go
+++ std/builtin/map.go
@@ -6,8 +6,8 @@ import (
 
 // ExtractStringKeys extracts keys from a map containing string keys. The
 // function uses reflection to extract keys from the map.
-func ExtractStringKeys(map_ interface{}) []string {
-	keys := reflect.ValueOf(map_).MapKeys()
+func ExtractStringKeys(stringmap interface{}) []string {
+	keys := reflect.ValueOf(stringmap).MapKeys()
 	result := make([]string, len(keys))
 	for i, v := range keys {
 		result[i] = v.Interface().(string)
