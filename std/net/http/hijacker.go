3a913e4d7931e402f5c50b088df068507e1654f7
Danny van Heumen <danny@dannyvanheumen.nl>
2020-02-05 20:06:21 +0100

    Hijack: expose bufio.ReadWriter only as io.Reader.

--- std/net/http/hijacker.go
+++ std/net/http/hijacker.go
@@ -1,8 +1,8 @@
 package http
 
 import (
-	"bufio"
 	"errors"
+	"io"
 	"net"
 	"net/http"
 )
@@ -15,7 +15,7 @@ var ErrNonHijackableWriter = errors.New("failed to acquire raw client connection
 // communication operations. The net.Conn instance is provided for low-level
 // connection maintenance.
 // The user must close net.Conn!
-func HijackConnection(resp http.ResponseWriter) (*bufio.ReadWriter, net.Conn, error) {
+func HijackConnection(resp http.ResponseWriter) (io.Reader, net.Conn, error) {
 	hijacker, ok := resp.(http.Hijacker)
 	if !ok {
 		return nil, nil, ErrNonHijackableWriter
