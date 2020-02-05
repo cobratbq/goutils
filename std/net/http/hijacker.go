7db59dfd697e85fe3d8e47240ec49197dfbe4b0a
Danny van Heumen <danny@dannyvanheumen.nl>
2020-02-05 14:50:28 +0100

    Migrated connection hijacking to goutils.

---
+++ std/net/http/hijacker.go
@@ -0,0 +1,24 @@
+package http
+
+import (
+	"errors"
+	"io"
+	"net"
+	"net/http"
+)
+
+// ErrNonHijackableWriter is returned when the connection cannot be hijacked.
+var ErrNonHijackableWriter = errors.New("failed to acquire raw client connection: writer is not hijackable")
+
+// HijackConnection acquires the underlying connection by inspecting the
+// ResponseWriter provided. One may use the returned io.ReadWriter to perform
+// communication operations. The net.Conn instance is provided for low-level
+// connection maintenance. The user must close net.Conn!
+func HijackConnection(resp http.ResponseWriter) (io.ReadWriter, net.Conn, error) {
+	hijacker, ok := resp.(http.Hijacker)
+	if !ok {
+		return nil, nil, ErrNonHijackableWriter
+	}
+	clientConn, bufrw, err := hijacker.Hijack()
+	return bufrw, clientConn, err
+}
