f643494121480cabb334b8ce7c2b769be3c8ab4d
Danny van Heumen <danny@dannyvanheumen.nl>
2020-02-21 18:25:59 +0100

    Check status code when downloading content.

--- std/net/http/download.go
+++ std/net/http/download.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -8,6 +9,8 @@ import (
 	io_ "github.com/cobratbq/goutils/std/io"
 )
 
+// DownloadToFile downloads content from the given URL into the specified file
+// name.
 func DownloadToFile(fileName, URL string) error {
 	dstFile, err := os.Create(fileName)
 	if err != nil {
@@ -17,6 +20,9 @@ func DownloadToFile(fileName, URL string) error {
 	return DownloadFromURL(dstFile, URL)
 }
 
+// DownloadFromURL downloads content from the specified URL and immediately
+// writes it to the destination. In case an unexpected http status code is
+// received, an error is returned containing that status code.
 func DownloadFromURL(dst io.Writer, url string) error {
 	resp, err := http.DefaultClient.Get(url)
 	if err != nil {
@@ -24,5 +30,19 @@ func DownloadFromURL(dst io.Writer, url string) error {
 	}
 	defer io_.ClosePanicked(resp.Body, "failed to close response body: %+v")
 	_, err = io.Copy(dst, resp.Body)
-	return err
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return ErrStatusCode(resp.StatusCode)
+	}
+	return nil
+}
+
+// ErrStatusCode indicates that a status code other than the expected status
+// code is received.
+type ErrStatusCode int
+
+func (code ErrStatusCode) Error() string {
+	return fmt.Sprintf("http status code: %d", code)
 }
