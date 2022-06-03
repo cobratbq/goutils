c9a7e1513b940cf4a3eb93853294a40e187240f5
Danny van Heumen <danny@dannyvanheumen.nl>
2022-06-03 20:47:35 +0200

    strings: HasPrefixes renamed AnyPrefix

--- std/strings/strings.go
+++ std/strings/strings.go
@@ -3,7 +3,7 @@ package strings
 
 import "strings"
 
-func HasAnyPrefix(s string, prefixes ...string) bool {
+func AnyPrefix(s string, prefixes ...string) bool {
 	for _, p := range prefixes {
 		if strings.HasPrefix(s, p) {
 			return true
