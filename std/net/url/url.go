f8f82c09a8372e8e1855a5111bba9c7aac95078d
Danny van Heumen <danny@dannyvanheumen.nl>
2022-06-03 20:50:22 +0200

    url: PortOrProtocolDefault() added

---
+++ std/net/url/url.go
@@ -0,0 +1,28 @@
+package url
+
+import (
+	"errors"
+	"net/url"
+	"strings"
+)
+
+var ErrSchemeMissing error = errors.New("scheme missing")
+var ErrSchemeUnknown error = errors.New("scheme unknown")
+
+func PortOrProtocolDefault(uri *url.URL) (string, error) {
+	port := uri.Port()
+	if port != "" {
+		return port, nil
+	}
+	scheme := strings.ToLower(uri.Scheme)
+	switch scheme {
+	case "":
+		return "", ErrSchemeMissing
+	case "http":
+		return "80", nil
+	case "https":
+		return "443", nil
+	default:
+		return "", ErrSchemeUnknown
+	}
+}
