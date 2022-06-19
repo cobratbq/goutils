c1848834cbc5d9ee9ec87cf3b10b24651d370bf9
Danny van Heumen <danny@dannyvanheumen.nl>
2022-06-19 21:01:33 +0200

    assert: added Equal

--- assert/assert.go
+++ assert/assert.go
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 package assert
 
-// TODO add Equal(a, b)
-
 func False(expected bool) {
-	True(!expected)
+	if expected {
+		panic("assertion failed: False")
+	}
 }
 
 func True(expected bool) {
 	if !expected {
-		panic("assertion failed")
+		panic("assertion failed: True")
+	}
+}
+
+func Equal[T comparable](v1, v2 T) {
+	if v1 != v2 {
+		panic("assertion failed: Equal")
 	}
 }
