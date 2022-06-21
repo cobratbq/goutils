3601ff04ab3e93da103bc7fd1418cd7b0d9904b2
Danny van Heumen <danny@dannyvanheumen.nl>
2022-06-21 22:30:26 +0200

    errors: extending on context/errors

--- std/errors/context.go
+++ std/errors/context.go
@@ -1,6 +1,10 @@
 package errors
 
+import "errors"
+
 // TODO consider dropping in favor of 'github.com/pkg/errors', although unfortunate that it is not part of stdlib
+
+// TODO consider defining different variations of context: with-message, with-key-values-pairs, ...
 
 // Context wraps an error to provide additional context information.
 func Context(cause error, message string) error {
@@ -19,4 +23,8 @@ func (c context) Error() string {
 
 func (c context) Unwrap() error {
 	return c.cause
+}
+
+func Is(err error, target error) bool {
+	return errors.Is(err, target)
 }
