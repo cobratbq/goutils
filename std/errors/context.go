d005f6ae66f4e64a1876ac9478e21965da17a1e3
Danny van Heumen <danny@dannyvanheumen.nl>
2022-07-04 21:25:32 +0200

    goutils: add wrapper for stack traces

--- std/errors/context.go
+++ std/errors/context.go
@@ -1,10 +1,17 @@
 package errors
 
-import "errors"
+import (
+	"errors"
+	"runtime/debug"
+)
+
+func ContextStacktrace(cause error, message string) context {
+	return Context(stack{cause: cause, stack: debug.Stack()}, message)
+}
 
 // Context wraps an error to provide additional context information.
 // TODO consider defining different variations of context: with-message, with-key-values-pairs, ...
-func Context(cause error, message string) error {
+func Context(cause error, message string) context {
 	// TODO should we return the pointer instead?
 	return context{cause: cause, message: message}
 }
@@ -20,6 +27,26 @@ func (c context) Error() string {
 
 func (c context) Unwrap() error {
 	return c.cause
+}
+
+// TODO think about how we want to unwind and print stack trace, because wrapping multiple contexts means that we might produce multiple stack traces, of which the inner-most one is most accurate.
+
+type stack struct {
+	cause error
+	stack []byte
+}
+
+func (s stack) Error() string {
+	// FIXME continue here
+	panic("to be implemented")
+}
+
+func (s stack) Unwrap() error {
+	return s.cause
+}
+
+func (s stack) Stack() []byte {
+	return s.stack
 }
 
 // Is repeatedly unwraps an error and compares to target on each unwrapping.
