a84700e6f8b96c8f5c411a5480dcc3e5f4aad805
Danny van Heumen <danny@dannyvanheumen.nl>
2022-07-16 21:19:47 +0200

    errors: remove outdated TODOs

--- std/errors/stack.go
+++ std/errors/stack.go
@@ -13,15 +13,12 @@ func Stacktrace(cause error) error {
 	return stack{cause: cause, stack: debug.Stack()}
 }
 
-// TODO think about how we want to unwind and print stack trace, because wrapping multiple contexts means that we might produce multiple stack traces, of which the inner-most one is most accurate.
-
 type stack struct {
 	cause error
 	stack []byte
 }
 
 func (s stack) Error() string {
-	// FIXME should we, and how, include the stacktrace in the error string?
 	return s.cause.Error() + "\n" + string(s.stack)
 }
 
