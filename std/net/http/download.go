1fe911161b615bfe9d4fe2ead86f01a02cb81c16
Danny van Heumen <danny@dannyvanheumen.nl>
2022-07-20 17:39:12 +0200

    http: return status code as well as error

--- std/net/http/download.go
+++ std/net/http/download.go
@@ -13,36 +13,37 @@ import (
 
 // DownloadToFilePath downloads content from the given URL into the specified file name. In case of
 // failure to download, the file is removed.
-func DownloadToFilePath(fileName, URL string) error {
+func DownloadToFilePath(fileName, URL string) (int, error) {
 	dstFile, err := os.Create(fileName)
 	if err != nil {
-		return err
+		return 0, err
 	}
 	defer io_.ClosePanicked(dstFile, "failed to close destination file: %+v")
-	if err = DownloadFromURL(dstFile, URL); err != nil {
+	if code, err := DownloadFromURL(dstFile, URL); err == nil {
+		return code, nil
+	} else {
 		os.Remove(dstFile.Name())
-		return err
+		return code, err
 	}
-	return nil
 }
 
 // DownloadFromURL downloads content from the specified URL and immediately writes it to the
 // destination. In case an unexpected http status code is received, an error is returned containing
 // that status code.
-func DownloadFromURL(dst io.Writer, url string) error {
+func DownloadFromURL(dst io.Writer, url string) (int, error) {
 	resp, err := http.DefaultClient.Get(url)
 	if err != nil {
-		return err
+		return resp.StatusCode, err
 	}
 	defer io_.ClosePanicked(resp.Body, "failed to close response body: %+v")
 	_, err = io.Copy(dst, resp.Body)
 	if err != nil {
-		return err
+		return 0, err
 	}
 	if resp.StatusCode != http.StatusOK {
-		return errors.Context(ErrStatusCode, "unexpected status code")
+		return resp.StatusCode, errors.Context(ErrStatusCode, "unexpected status code")
 	}
-	return nil
+	return resp.StatusCode, nil
 }
 
 var ErrStatusCode = errors.NewStringError("status code error")
