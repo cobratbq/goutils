5977ce7a5ac030ca25d684ac62bdb87fbbbbb9d6
Danny van Heumen <danny@dannyvanheumen.nl>
2022-12-07 00:16:46 +0100

    builtin: add consts for int and uint for easy access

---
+++ std/builtin/const.go
@@ -0,0 +1,25 @@
+package builtin
+
+import (
+	"math"
+	"math/bits"
+	"strconv"
+)
+
+// IntSize is the bit-size of the data-type for the targeted platform.
+const IntSize = strconv.IntSize
+
+// MaxInt is the maximum int value for the targeted platform. Other constants for data-types can be
+// found in package `math` package.
+const MaxInt = math.MaxInt
+
+// MinInt is the minimum int value for the targeted platform. Other constants for data-types can be
+// found in package `math` package.
+const MinInt = math.MinInt
+
+// UintSize is the bit-size of the data-type for the targeted platform.
+const UintSize = bits.UintSize
+
+// MaxUint is the maximum uint value for the targeted platform. Other constants for data-types can
+// be found in package `math` package.
+const MaxUint = math.MaxUint
