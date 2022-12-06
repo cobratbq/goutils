3a60ae5c1feade22417c5ae290809af571c27c76
Danny van Heumen <danny@dannyvanheumen.nl>
2022-12-06 19:34:59 +0100

    multiset: added IncrementN and DecrementN

--- std/builtin/multiset/multiset.go
+++ std/builtin/multiset/multiset.go
@@ -2,11 +2,24 @@
 // of elements. Functions `Increment` and `Decrement` will increase or decrease the count of
 // elements of that key, and if the count reaches zero, it will delete the element from the map
 // (multiset). This allows testing the map with `len(multiset)` to test current size.
+//
+// The multiset uses `uint`-based counts such that the multiset adapts to the capacity of the
+// architecture on which it is executed.
+//
+// Invariant: all elements have a count strictly larger than 0. (I.e. elements that reach 0 must be
+// deleted from the multiset.)
 package multiset
 
-// Increment increments the presence count of an element, inserting the element if necessary.
+import "github.com/cobratbq/goutils/assert"
+
+// Increment increments the count of an element, inserting the element if necessary.
 func Increment[K comparable](multiset map[K]uint, e K) {
 	multiset[e] += 1
+}
+
+// IncrementN increments the count of an element with n, inserting the element if necessary.
+func IncrementN[K comparable](multiset map[K]uint, e K, n uint) {
+	multiset[e] += n
 }
 
 // Decrement decrements the presence count of an element, deleting the element from the map
@@ -15,6 +28,16 @@ func Increment[K comparable](multiset map[K]uint, e K) {
 // default allows transparently querying non-existing element and receiving an accurate count of 0.
 func Decrement[K comparable](multiset map[K]uint, e K) {
 	multiset[e] -= 1
+	if multiset[e] == 0 {
+		delete(multiset, e)
+	}
+}
+
+// DecrementN decrements the count of an element by `n`. The element count must be at least `n`,
+// otherwise the function panics.
+func DecrementN[K comparable](multiset map[K]uint, e K, n uint) {
+	assert.Require(n <= multiset[e], "Decrement count cannot be bigger than count in multiset.")
+	multiset[e] -= n
 	if multiset[e] == 0 {
 		delete(multiset, e)
 	}
