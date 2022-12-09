c5460c652ee43d125e078dab6ea49b64b05e05f2
Danny van Heumen <danny@dannyvanheumen.nl>
2022-12-09 19:36:59 +0100

    encoding/digit: added test for trivial conversion testing

---
+++ encoding/bytes/digit/digit_test.go
@@ -0,0 +1,16 @@
+package digit
+
+import (
+	"testing"
+
+	"github.com/cobratbq/goutils/assert"
+)
+
+func TestReadWriteDigit(t *testing.T) {
+	for i := uint8(0); i < 10; i++ {
+		assert.Equal(i, ReadDigit(WriteDigit(i)))
+	}
+	for c := byte('0'); c <= '9'; c++ {
+		assert.Equal(c, WriteDigit(ReadDigit(c)))
+	}
+}
