70220c55fd8d3cc6755c8e6343f61623b9daeb7d
Danny van Heumen <danny@dannyvanheumen.nl>
2022-12-11 02:56:31 +0100

    Moved Expect/Error funcs to builtin

--- std/strconv/parse.go
+++ std/strconv/parse.go
@@ -5,17 +5,17 @@ package strconv
 import (
 	"strconv"
 
-	"github.com/cobratbq/goutils/assert"
+	"github.com/cobratbq/goutils/std/builtin"
 )
 
 // MustParseInt parses a string for an integer value of at most specified
 // bitsize. Success is assumed and the function will panic on error.
 func MustParseInt(v string, base, bitsize int) int64 {
-	return assert.Expect(strconv.ParseInt(v, base, bitsize))
+	return builtin.Expect(strconv.ParseInt(v, base, bitsize))
 }
 
 // MustParseUint parses a string for an unsigned integer value of at most specified bitsize. Success
 // is assumed and the function will panic on error.
 func MustParseUint(v string, base, bitsize int) uint64 {
-	return assert.Expect(strconv.ParseUint(v, base, bitsize))
+	return builtin.Expect(strconv.ParseUint(v, base, bitsize))
 }
