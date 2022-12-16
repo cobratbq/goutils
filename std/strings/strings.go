d775dfcb131650e36d6301d69dd790d4d7f960be
Danny van Heumen <danny@dannyvanheumen.nl>
2022-12-17 00:02:57 +0100

    strings: updated docs

--- std/strings/strings.go
+++ std/strings/strings.go
@@ -4,6 +4,7 @@ package strings
 
 import "strings"
 
+// AnyPrefix tests for any of series of prefixes.
 func AnyPrefix(s string, prefixes ...string) bool {
 	for _, p := range prefixes {
 		if strings.HasPrefix(s, p) {
