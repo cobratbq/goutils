cfccc8ada10f0b661b84030b492477cf83e30250
Danny van Heumen <danny@dannyvanheumen.nl>
2022-12-17 18:03:42 +0100

    math: LCM: reorder math operations to prevent overflowing

--- std/math/math.go
+++ std/math/math.go
@@ -2,24 +2,26 @@ package math
 
 import "github.com/cobratbq/goutils/assert"
 
-// LCM_uint is a unoptimized version of the Least/Lowest Common Multiple.
+// LCM is a unoptimized version of the Least/Lowest Common Multiple.
 //
 // Using the Greatest Common Divisor, following [1]:
 // `lcm(a,b) = |ab| / gcd(a,b)`
 //
 // [1]: <https://en.wikipedia.org/wiki/Least_common_multiple>
-func LCM_uint(a, b uint) uint {
-	return (a * b) / GCD_uint(a, b)
+func LCM(a, b uint64) uint64 {
+	// NOTE: first divide `b` by `GCD` to prevent multiplication from greatly increasing the
+	// intermediate result with risk of overflowing.
+	return a * (b / GCD(a, b))
 }
 
-// GCD_uint is a unoptimized, inefficient version of the Greatest Common Divisor.
+// GCD is a unoptimized, inefficient version of the Greatest Common Divisor.
 //
 // Using Euclidian algorithm, following [1]:
 // `gcd(a, b) == gcd(b, a mod b)` for `a > b`. Perform repeatedly until `gcd(x, 0)`, at which point
 // `x` is greatest common divisor for `gcd(a,b)`.
 //
 // [1]: <https://en.wikipedia.org/wiki/Greatest_common_divisor>
-func GCD_uint(a, b uint) uint {
+func GCD(a, b uint64) uint64 {
 	assert.Require(a > 0, "Non-zero values required for this method of GCD calculation.")
 	assert.Require(b > 0, "Non-zero values required for this method of GCD calculation.")
 	if a == b {
