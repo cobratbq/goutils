f3836d312fda478016aaa662795c76f5f0173ad1
Danny van Heumen <danny@dannyvanheumen.nl>
2022-12-19 14:49:58 +0100

    set: func Contains to test for element

--- std/builtin/set/set.go
+++ std/builtin/set/set.go
@@ -4,6 +4,12 @@
 // allows the map to be used as a set, indicating whether an element is present (as a key) or not.
 package set
 
+// Contains tests for the presence of element `e` and returns true iff present.
+func Contains[K comparable](set map[K]struct{}, e K) bool {
+	_, ok := set[e]
+	return ok
+}
+
 // Insert inserts an element into the map (with the unit-value).
 func Insert[K comparable](set map[K]struct{}, e K) {
 	set[e] = struct{}{}
