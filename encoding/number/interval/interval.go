74daf757c69a5afe018d4677ceede6a7d3c2d953
Danny van Heumen <danny@dannyvanheumen.nl>
2023-01-10 22:58:52 +0100

    encoding/number/interval: expand range of values from closed interval points

---
+++ encoding/number/interval/interval.go
@@ -0,0 +1,19 @@
+package interval
+
+import (
+	"github.com/cobratbq/goutils/std/builtin"
+)
+
+func ExpandClosed[N builtin.Integer](a, b N) []N {
+	if a == b {
+		return []N{a}
+	}
+	var parts []N
+	for i := a; i <= b; i++ {
+		parts = append(parts, i)
+	}
+	for i := a; i >= b; i-- {
+		parts = append(parts, i)
+	}
+	return parts
+}
