56e92090ec57a450475f928de1e5d759dcc453c7
Danny van Heumen <danny@dannyvanheumen.nl>
2023-01-11 19:35:55 +0100

    assert: support build-tag "disable_assert"

---
+++ assert/assert_disabled.go
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+//go:build disable_assert
+
+package assert
+
+func False(expected bool) {}
+
+func True(expected bool) {}
+
+func Any[T comparable](actual T, values ...T) {}
+
+func Equal[T comparable](v1, v2 T) {}
--- assert/require_disabled.go
+++ assert/require_disabled.go
@@ -1,49 +1,27 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-//go:build !disable_assert
+//go:build disable_assert
 
 package assert
-
-import (
-	"log"
-)
 
 // RequireSuccess checks that err is nil. If the error is non-nil, it will panic. `message` can have
 // '%v' format specifier so that it can be substituted with the error message.
 // Deprecated: `RequireSuccess` deprecated in favor of `Success`.
-func RequireSuccess(err error, message string) {
-	log.Println("assert.RequireSuccess is deprecated. Use assert.Success")
-	Success(err, message)
-}
+func RequireSuccess(err error, message string) {}
 
 // Success checks that err is nil. If the error is non-nil, it will panic. `message` can have '%v'
 // format specifier so that it can be substituted with the error message.
-func Success(err error, message string) {
-	if err == nil {
-		return
-	}
-	panic(message + ": " + err.Error())
-}
+func Success(err error, message string) {}
 
 // Required checks if provided value is nil, if so panics with provided message.
-func Required(val any, message string) {
-	Require(val != nil, message)
-}
+func Required(val any, message string) {}
 
 // Require check required condition and panics if condition does not hold.
-func Require(condition bool, message string) {
-	if !condition {
-		panic(message)
-	}
-}
+func Require(condition bool, message string) {}
 
 // Unreachable panics with a message to indicate this should not be happening.
 // TODO how useful is this? Go compiler/static analysis cannot detect that this function call is terminal
-func Unreachable() {
-	panic("BUG: this code should not be reachable.")
-}
+func Unreachable() {}
 
 // Unsupported panics with the provided message in order to signal for an unsupported case.
-func Unsupported(message string) {
-	panic("Unsupported: " + message)
-}
+func Unsupported(message string) {}
