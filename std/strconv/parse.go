2ba9e05748a2215e0efb5216bccb719436ce7898
Danny van Heumen <danny@dannyvanheumen.nl>
2023-01-16 23:43:40 +0100

    strconv: ParseConsecutiveDigits for parsing unsigned integer

--- std/strconv/parse.go
+++ std/strconv/parse.go
@@ -4,9 +4,36 @@ package strconv
 
 import (
 	"strconv"
+	"unsafe"
 
+	"github.com/cobratbq/goutils/encoding/bytes/digit"
 	"github.com/cobratbq/goutils/std/builtin"
+	"github.com/cobratbq/goutils/types"
 )
+
+// ParseConsecutiveDigits reads bytes from the line until a non-digit is found, then parses the
+// previous bytes in order to return an unsigned integer and the number of bytes read. Note that
+// produced values will always be unsigned, due to accepting digits only.
+//
+// If no digits are found, this is not considered an error. Instead, `0, 0` is returned to
+// accurately represent the input.
+//
+// If parsing the digits produces an error, this error is returned.
+func ParseConsecutiveDigits[T types.UnsignedInteger](line []byte) (T, int) {
+	var i int
+	for len(line) > i && digit.IsDigit(line[i]) {
+		i++
+	}
+	if i == 0 {
+		return 0, 0
+	}
+	// TODO ideally, we get a constant expression, but this is not possible within the function.
+	unsigned := T(MustParseUint(string(line[:i]), DecimalBase, int(unsafe.Sizeof(T(0)))*8))
+	return unsigned, i
+}
+
+// DecimalBase is the base for the decimal system.
+const DecimalBase = 10
 
 // MustParseInt parses a string for an integer value of at most specified
 // bitsize. Success is assumed and the function will panic on error.
--- std/strconv/parse_test.go
+++ std/strconv/parse_test.go
@@ -4,6 +4,8 @@ package strconv
 
 import (
 	"testing"
+
+	assert "github.com/cobratbq/goutils/std/testing"
 )
 
 func TestMustParseIntEmptyString(t *testing.T) {
@@ -25,5 +27,28 @@ func TestMustParseIntIllegalString(t *testing.T) {
 func TestMustParseIntZero(t *testing.T) {
 	if MustParseInt("0", 10, 64) != 0 {
 		t.FailNow()
+	}
+}
+
+func TestParseConsecutiveDigitsNil(t *testing.T) {
+	testdata := []struct {
+		input []byte
+		val   uint64
+		n     int
+	}{
+		{nil, 0, 0},
+		{[]byte{}, 0, 0},
+		{[]byte("0"), 0, 1},
+		{[]byte("9"), 9, 1},
+		{[]byte("0000"), 0, 4},
+		{[]byte("0000a"), 0, 4},
+		{[]byte("999"), 999, 3},
+		{[]byte("3a2b1c"), 3, 1},
+		{[]byte("a2b1c"), 0, 0},
+	}
+	for _, d := range testdata {
+		val, n := ParseConsecutiveDigits[uint64](d.input)
+		assert.Equal(t, d.val, val)
+		assert.Equal(t, d.n, n)
 	}
 }
