7861810158ad91d7f8db784390034f11651fc382
Danny van Heumen <danny@dannyvanheumen.nl>
2023-01-24 21:21:52 +0100

    builtin: test value for any out of provided sequence

---
+++ std/builtin/comparable.go
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package builtin
+
+// AnyOf matches the specified value with any of the provided `matches` values. It returns `true` if
+// it is any of the provided matches, or `false` if none match.
+func AnyOf[T comparable](value T, matches ...T) bool {
+	for _, m := range matches {
+		if value == m {
+			return true
+		}
+	}
+	return false
+}
