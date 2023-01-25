9031be97ff71a22a0323270b202d5a76d26146e2
Danny van Heumen <danny@dannyvanheumen.nl>
2023-01-25 01:12:55 +0100

    builtin: rename AnyOf to EqualsAny for consistency

--- std/builtin/comparable.go
+++ std/builtin/comparable.go
@@ -2,9 +2,9 @@
 
 package builtin
 
-// AnyOf matches the specified value with any of the provided `matches` values. It returns `true` if
-// it is any of the provided matches, or `false` if none match.
-func AnyOf[T comparable](value T, matches ...T) bool {
+// EqualsAny matches the specified value with any of the provided `matches` values. It returns
+// `true` if it is any of the provided matches, or `false` if none match.
+func EqualsAny[T comparable](value T, matches ...T) bool {
 	for _, m := range matches {
 		if value == m {
 			return true
