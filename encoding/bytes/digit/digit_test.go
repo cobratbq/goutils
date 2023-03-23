b984fd06fe1bfcbcdeba98dcf0d78294202032ab
Danny van Heumen <danny@dannyvanheumen.nl>
2023-03-23 02:53:12 +0100

    encoding/bytes/digit: fix tests :-P

--- encoding/bytes/digit/digit_test.go
+++ encoding/bytes/digit/digit_test.go
@@ -8,11 +8,11 @@ import (
 	"github.com/cobratbq/goutils/assert"
 )
 
-func TestReadWriteDigit(t *testing.T) {
+func TestEncodeDecodeDigit(t *testing.T) {
 	for i := uint8(0); i < 10; i++ {
-		assert.Equal(i, ReadDigit(WriteDigit(i)))
+		assert.Equal(i, DecodeDigit(EncodeDigit(i)))
 	}
 	for c := byte('0'); c <= '9'; c++ {
-		assert.Equal(c, WriteDigit(ReadDigit(c)))
+		assert.Equal(c, EncodeDigit(DecodeDigit(c)))
 	}
 }
