4748f6c05bf8e94c406bde8d1f13c2a40a70f1be
Danny van Heumen <danny@dannyvanheumen.nl>
2023-03-28 00:34:48 +0200

    std/encoding: document separated encoding package

---
+++ std/encoding/doc.go
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+// encoding exists at `/encoding` of goutils, as encodings have significantly different properties
+// from "plain" utils.
+package encoding
