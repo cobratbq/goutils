221e5e48487d323b283eee267e85b3aaa174e331
Danny van Heumen <danny@dannyvanheumen.nl>
2023-03-28 00:34:19 +0200

    time: use constant

--- std/time/fmt.go
+++ std/time/fmt.go
@@ -5,8 +5,10 @@ package time
 import (
 	"strconv"
 	"time"
+
+	strconv_ "github.com/cobratbq/goutils/std/strconv"
 )
 
 func NanotimeAsStringHex(t time.Time) string {
-	return strconv.FormatInt(t.UnixNano(), 16)
+	return strconv.FormatInt(t.UnixNano(), strconv_.HexadecimalBase)
 }
