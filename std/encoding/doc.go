8362ffbeffc03f68aac314873eaadf9c5e623227
Danny van Heumen <danny@dannyvanheumen.nl>
2023-03-28 21:05:34 +0200

    std/encoding: just removed the package, to avoid confusion

--- std/encoding/doc.go
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-// encoding exists at `/encoding` of goutils, as encodings have significantly different properties
-// from "plain" utils.
-package encoding
