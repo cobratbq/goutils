7127cb0a596d098099bcb69aa0e292bca4752ee5
Danny van Heumen <danny@dannyvanheumen.nl>
2023-12-01 18:37:26 +0100

    big-,littleendian: FIXME concerning stdlib binary package

---
+++ encoding/bytes/bigendian/doc.go
@@ -0,0 +1,2 @@
+// FIXME this is redundant: "binary".BigEndian provides the same functionality (mostly). See if we can replace or synergize.
+package bigendian
---
+++ encoding/bytes/littleendian/doc.go
@@ -0,0 +1,2 @@
+// FIXME this is redundant: "binary".LittleEndian provides the same functionality (mostly). See if we can replace or synergize.
+package littleendian
