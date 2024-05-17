9834c0cf6104471e97173c7dffdea912aef5801e
Danny van Heumen <danny@dannyvanheumen.nl>
2024-05-17 19:49:33 +0200

    goutils: codec/bytes/varint added for Bitcoin VarInt

---
+++ codec/bytes/varint/doc.go
@@ -0,0 +1,8 @@
+// Bitcoin VarInt encoding, used internally in Bitcoin Core. (Not part of specification.)
+//
+// ref: <https://learnmeabitcoin.com/technical/general/compact-size/#varint>
+// ref: <https://github.com/in3rsha/bitcoin-chainstate-parser/blob/master/README.md#varints>
+// ref: <https://protobuf.dev/programming-guides/encoding/#varints>
+// ref: <https://github.com/bitcoin-core/gui-qml/blob/main/src/serialize.h#L373>
+// TODO implement Bitcoin 'VarInt' en-/decoding
+package varint
