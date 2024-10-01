2b392cd0a04f7bd75047a1f7559be03ab8853e87
Danny van Heumen <danny@dannyvanheumen.nl>
2024-10-01 03:03:59 +0200

    base58: correct test after changing base58.CheckDecode

--- codec/string/base58/codec_test.go
+++ codec/string/base58/codec_test.go
@@ -28,9 +28,9 @@ func TestDecodeBase58Check(t *testing.T) {
 		data    []byte
 		decoded []byte
 	}{
-		{decoded: builtin.Expect(hex.DecodeString("00f54a5851e9372b87810a8e60cdd2e7cfd80b6e31c7f18fe8")), data: []byte("1PMycacnJaSqwwJqjawXBErnLsZ7RkXUAs")},
-		{decoded: builtin.Expect(hex.DecodeString("800C28FCA386C7A227600B2FE50B7CAE11EC86D3BF1FBE471BE89827E19D72AA1D507A5B8D")), data: []byte("5HueCGU8rMjxEXxiPuD5BDku4MkFqeZyd4dZ1jvhTVqvbTLvyTJ")},
-		{data: []byte("1MF4G8etz6mPmpa9RST2DPHQnFsCraM5fm"), decoded: []byte{0, 222, 11, 15, 170, 194, 21, 170, 57, 157, 119, 67, 31, 32, 243, 195, 249, 233, 84, 252, 243, 242, 135, 232, 232}},
+		{decoded: builtin.Expect(hex.DecodeString("00f54a5851e9372b87810a8e60cdd2e7cfd80b6e31")), data: []byte("1PMycacnJaSqwwJqjawXBErnLsZ7RkXUAs")},
+		{decoded: builtin.Expect(hex.DecodeString("800C28FCA386C7A227600B2FE50B7CAE11EC86D3BF1FBE471BE89827E19D72AA1D")), data: []byte("5HueCGU8rMjxEXxiPuD5BDku4MkFqeZyd4dZ1jvhTVqvbTLvyTJ")},
+		{data: []byte("1MF4G8etz6mPmpa9RST2DPHQnFsCraM5fm"), decoded: []byte{0, 222, 11, 15, 170, 194, 21, 170, 57, 157, 119, 67, 31, 32, 243, 195, 249, 233, 84, 252, 243}},
 	}
 	for _, entry := range testdata {
 		result, err := CheckDecode(entry.data)
