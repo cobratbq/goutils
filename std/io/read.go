423f4069049abfa95b70021a449795f0428001d3
Danny van Heumen <danny@dannyvanheumen.nl>
2024-10-01 03:00:59 +0200

    std/io: added ReadUntil and ReadExpect for one-off use where bufio is overkill

--- std/io/read.go
+++ std/io/read.go
@@ -60,3 +60,37 @@ func MustReadBytes(in io.Reader, dst []byte) {
 	assert.Success(err, "failed to read random bytes")
 	assert.Equal(n, len(dst))
 }
+
+// ReadExpect reads and checks if the read byte is the expected `next` byte.
+func ReadExpect(next byte, in io.Reader) (bool, error) {
+	var b [1]byte
+	if n, err := io.ReadFull(in, b[:]); err != nil {
+		return false, errors.Context(err, "Failed to read next byte")
+	} else if n == 0 {
+		return false, errors.Context(io.ErrShortWrite, "Failed to read next byte")
+	}
+	return b[0] == next, nil
+}
+
+// ReadUntil reads until a specific byte is encountered.
+// All bytes read before the stop-byte are returned. If an error is encountered, everything read until the
+// error is returned together with the error.
+func ReadUntil(stop byte, in io.Reader) ([]byte, error) {
+	var buffer []byte
+	var b [1]byte
+	var n int
+	var err error
+	for {
+		if n, err = io.ReadFull(in, b[:]); err != nil {
+			return buffer, err
+		}
+		if n == 0 {
+			continue
+		}
+		if b[0] == stop {
+			break
+		}
+		buffer = append(buffer, b[0])
+	}
+	return buffer, nil
+}
