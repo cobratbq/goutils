4f5f202302cf3d3e28c8833b648064d5dc5d28fe
Danny van Heumen <danny@dannyvanheumen.nl>
2024-11-21 19:10:42 +0100

    big-,littleendian: return arrays instead of slices

--- codec/bytes/littleendian/write.go
+++ codec/bytes/littleendian/write.go
@@ -11,35 +11,38 @@ func WriteUint8(out io.Writer, value uint8) error {
 }
 
 func WriteUint16(out io.Writer, value uint16) error {
-	_, err := out.Write(FromUint16(value))
+	encoded := FromUint16(value)
+	_, err := out.Write(encoded[:])
 	return err
 }
 
-func FromUint16(value uint16) []byte {
+func FromUint16(value uint16) [2]byte {
 	b0 := uint8(0x00ff & value)
 	b1 := uint8((0xff00 & value) >> 8)
-	return []byte{b0, b1}
+	return [...]byte{b0, b1}
 }
 
 func WriteUint32(out io.Writer, value uint32) error {
-	_, err := out.Write(FromUint32(value))
+	encoded := FromUint32(value)
+	_, err := out.Write(encoded[:])
 	return err
 }
 
-func FromUint32(value uint32) []byte {
+func FromUint32(value uint32) [4]byte {
 	b0 := uint8(0x000000ff & value)
 	b1 := uint8((0x0000ff00 & value) >> 8)
 	b2 := uint8((0x00ff0000 & value) >> 16)
 	b3 := uint8((0xff000000 & value) >> 24)
-	return []byte{b0, b1, b2, b3}
+	return [...]byte{b0, b1, b2, b3}
 }
 
 func WriteUint64(out io.Writer, value uint64) error {
-	_, err := out.Write(FromUint64(value))
+	encoded := FromUint64(value)
+	_, err := out.Write(encoded[:])
 	return err
 }
 
-func FromUint64(value uint64) []byte {
+func FromUint64(value uint64) [8]byte {
 	b0 := uint8(0x00000000000000ff & value)
 	b1 := uint8((0x000000000000ff00 & value) >> 8)
 	b2 := uint8((0x0000000000ff0000 & value) >> 16)
@@ -48,5 +51,5 @@ func FromUint64(value uint64) []byte {
 	b5 := uint8((0x0000ff0000000000 & value) >> 40)
 	b6 := uint8((0x00ff000000000000 & value) >> 48)
 	b7 := uint8((0xff00000000000000 & value) >> 56)
-	return []byte{b0, b1, b2, b3, b4, b5, b6, b7}
+	return [...]byte{b0, b1, b2, b3, b4, b5, b6, b7}
 }
