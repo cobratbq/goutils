8f7184f7e1dcec3472ba8aa2975bf48092269719
Danny van Heumen <danny@dannyvanheumen.nl>
2024-12-27 17:46:02 +0100

    codec:prefixed-compact: added test with heterogeneous map-value

--- codec/bytes/prefixed-compact/verify_test.go
+++ codec/bytes/prefixed-compact/verify_test.go
@@ -13,3 +13,24 @@ func TestVerify(t *testing.T) {
 	err := Verify(bytes.NewReader([]byte{}))
 	assert.Nil(t, err)
 }
+
+func TestVerifyBasicMap(t *testing.T) {
+	var b bytes.Buffer
+	_, err := WriteMap(&b, map[string]Value{"test1": Bytes("hello"), "test2": Bytes("world")})
+	assert.Nil(t, err)
+	err = Verify(&b)
+	assert.Nil(t, err)
+	assert.Equal(t, 0, b.Len())
+}
+
+func TestVerifyMixedMap(t *testing.T) {
+	var b bytes.Buffer
+	_, err := WriteMap(&b, map[string]Value{
+		"test1": Bytes("hello"),
+		"test2": &KeyValue{"id", Bytes("world")},
+		"test3": SequenceValue{Bytes("a"), Bytes("b")}})
+	assert.Nil(t, err)
+	err = Verify(&b)
+	assert.Nil(t, err)
+	assert.Equal(t, 0, b.Len())
+}
