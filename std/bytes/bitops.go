f6adc45d5f8719b33aa2a0e702ac1c8ccb847b03
Danny van Heumen <danny@dannyvanheumen.nl>
2025-01-27 03:42:03 +0100

    std/bytes: fix ShiftLeft, add ShiftRight

--- std/bytes/bitops.go
+++ std/bytes/bitops.go
@@ -17,6 +17,7 @@ func ShiftLeft(slice []byte, n int) {
 	if len(slice) == 0 || n == 0 {
 		return
 	}
+	// Skipping/shifting full bytes
 	if skip := n / 8; skip > 0 {
 		for i := range slice {
 			if i < skip {
@@ -24,17 +25,50 @@ func ShiftLeft(slice []byte, n int) {
 			}
 			slice[i-skip] = slice[i]
 		}
-		slice[len(slice)-1] = 0
+		// TODO is this properly implemented to zero the right-most full bytes after shifting?
+		for i := 0; i < skip && len(slice)-1-i >= 0; i++ {
+			slice[len(slice)-1-i] = 0
+		}
 		n -= skip * 8
 	}
 	assert.AtMost(7, n)
 	if n == 0 {
 		return
 	}
+	// Shifting remaining bits
 	for i := range slice {
 		if i > 0 {
 			slice[i-1] |= byte(slice[i]&byte(0b11111111<<(8-n))) >> (8 - n)
 		}
 		slice[i] <<= n
+	}
+}
+
+// FIXME needs testing
+func ShiftRight(slice []byte, n int) {
+	assert.NonNegative(n)
+	if len(slice) == 0 || n == 0 {
+		return
+	}
+	// skipping/shifting full bytes
+	if skip := n / 8; skip > 0 {
+		for i := len(slice) - 1; i >= skip; i-- {
+			slice[i] = slice[i-skip]
+		}
+		for i := 0; i < skip && i < len(slice); i++ {
+			slice[i] = 0
+		}
+		n -= skip * 8
+	}
+	assert.AtMost(7, n)
+	if n == 0 {
+		return
+	}
+	// Shifting remaining bits
+	for i := len(slice) - 1; i >= 0; i-- {
+		slice[i] >>= n
+		if i > 0 {
+			slice[i] |= byte(slice[i-1]&byte(0b11111111>>(8-n))) << (8 - n)
+		}
 	}
 }
