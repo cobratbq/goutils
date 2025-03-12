12c19e36a139162d633c11c25f82300fa30bbf91
Danny van Heumen <danny@dannyvanheumen.nl>
2025-03-12 22:08:24 +0100

    asn1: rename ConvertP1363 to FromP1363

--- std/encoding/asn1/signature.go
+++ std/encoding/asn1/signature.go
@@ -7,10 +7,10 @@ import (
 	"github.com/cobratbq/goutils/assert"
 )
 
-// ConvertP1363 converts a P1363-formatted signature into DER-format (ASN.1-encoded).
+// FromP1363 converts a P1363-formatted signature into DER-format (ASN.1-encoded).
 // `signature` is expected to consist of two equal-length components `R` and `s`, and exact slice-length is
 // needed.
-func ConvertP1363(signature []byte) []byte {
+func FromP1363(signature []byte) []byte {
 	assert.Equal(0, len(signature)%2)
 	var compLength = len(signature) / 2
 	r := new(big.Int).SetBytes(signature[:compLength]).Bytes()
