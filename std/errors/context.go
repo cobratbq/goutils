3a8c59941bf555a0a387ff661aef8a44e60e9c07
Danny van Heumen <danny@dannyvanheumen.nl>
2025-07-23 19:29:05 +0200

    std:errors: change Aggregate(..) to embed errors themselves

--- std/errors/context.go
+++ std/errors/context.go
@@ -6,17 +6,30 @@ import (
 	"github.com/cobratbq/goutils/assert"
 )
 
-// Aggregate creates a context for a root cause, based on any number of errors.
-// TODO consider if we need to embed the actual errors. This touches on a tangential consideration: how to handle errors crossing abstraction boundaries. Do we want to make them available, i.e. leak the abstraction?
+// Aggregate creates a context for a root cause, with any number of additional errors.
 func Aggregate(cause error, message string, errs ...error) error {
-	return Context(cause, message+" (["+JoinMessages(errs, "],[")+"])")
+	assert.Required(cause, "Primary cause cannot be nil")
+	return &contextN{message: message, causes: append([]error{cause}, errs...)}
+}
+
+type contextN struct {
+	causes  []error
+	message string
+}
+
+func (c *contextN) Error() string {
+	return c.message + " ([" + JoinMessages(c.causes, "],[") + "])"
+}
+
+func (c *contextN) Unwrap() []error {
+	return c.causes
 }
 
 // Context wraps an error to provide additional context information.
 // TODO consider defining different variations of context: with-message, with-key-values-pairs, ... We can consider this basic context type as key-value pair with key 'message'. Then as we extract key-value pairs, we can include base contexts.
 func Context(cause error, message string) error {
 	assert.Required(cause, "Context expects to wrap an error but got nil")
-	return context{cause: cause, message: message}
+	return &context{cause: cause, message: message}
 }
 
 type context struct {
@@ -24,10 +37,10 @@ type context struct {
 	message string
 }
 
-func (c context) Error() string {
+func (c *context) Error() string {
 	return c.message + ": " + c.cause.Error()
 }
 
-func (c context) Unwrap() error {
+func (c *context) Unwrap() error {
 	return c.cause
 }
